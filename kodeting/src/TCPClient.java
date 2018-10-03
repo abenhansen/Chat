c9ba164f6c6229d4396f83c8c2b47de0ab483237
Kenneth Leo Hansen <abenhansen@gmail.com>
2018-10-03 23:58:02 +0200

    Add files via upload

--- kodeting/src/TCPClient.java
+++ kodeting/src/TCPClient.java
@@ -1,4 +1,6 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -11,15 +13,15 @@ public class TCPClient {
         String Username;
 
         // Skal rykkes  til server
-        while (!in.hasNext("[A-Za-z0-9\\-\\_]+")) {
+        /*while (!in.hasNext("[A-Za-z0-9\\-\\_]+")) {
             System.out.println("username needs to be lower than 12 characters and have no symbols, please try again");
             Username = in.nextLine();
-        }
+        }*/
         Username = in.nextLine();
-        while (Username.length() > 12) {
+        /*while (Username.length() > 12) {
             System.out.println("username needs to be lower than 12 characters");
             Username = in.nextLine();
-        }
+        }*/
         System.out.println("Enter server IP to connect to");
         final String ipConnect = in.nextLine();
         System.out.println("Enter port port to connect to");
@@ -30,6 +32,24 @@ public class TCPClient {
             System.out.println("Connecting to the server");
             Socket clientSocket = new Socket (ip, portConnect);
 
+            InputStream inp = clientSocket.getInputStream();
+            OutputStream out = clientSocket.getOutputStream();
+            String userMsg = ("JOIN " + Username+ ", 127.0.0.1:5659\n");
+            byte[] userJoin = userMsg.getBytes();
+            out.write(userJoin);
+
+            in = new Scanner(System.in);
+            System.out.println("Message to server?");
+            String sendMessage = in.nextLine();
+            byte[] sendData = sendMessage.getBytes();
+            out.write(sendData);
+
+            byte[] dataRecieve = new byte[1024];
+            inp.read(dataRecieve);
+            String msgRecieve = new String(dataRecieve);
+            msgRecieve.trim();
+
+            System.out.println("IN -->" + msgRecieve + "<--");
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
--- kodeting/src/TCPServer.java
+++ kodeting/src/TCPServer.java
@@ -1,6 +1,53 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TCPServer {
     public static void main(String[] args) {
         final int servport =5659;
+
+        try{
+            ServerSocket serv = new ServerSocket(servport);
+
+            System.out.println("Starting the server!\n");
+
+            Socket sock =  serv.accept();
+            System.out.println("User has connected!");
+            String userIP = sock.getInetAddress().getHostAddress();
+            InputStream inp = sock.getInputStream();
+            OutputStream out = sock.getOutputStream();
+            while (true) {
+                System.out.println("IP: " + userIP);
+                System.out.println("PORT: " + sock.getPort());
+                byte[] userRecieve = new byte[1024];
+                inp.read(userRecieve);
+                String userResieveString = new String(userRecieve);
+                userResieveString.trim();
+                if (userResieveString.length() > 12) {
+                    System.out.println("test");
+                    continue;
+                } else if (userResieveString.contains("[A-Za-z0-9\\-\\_]")) {
+                    continue;
+                } else
+                    System.out.println("Username: " + userResieveString);
+                break;
+
+            }
+            byte[] dataRecieve = new byte[1024];
+            inp.read(dataRecieve);
+            String msgRecieve = new String(dataRecieve);
+            msgRecieve.trim();
+
+            System.out.println("IN -->" + msgRecieve + "<--");
+
+            String sendMessage = "SERVER: [sender:" + userIP + " ]: " + msgRecieve;
+            byte[] sendData = sendMessage.getBytes();
+            out.write(sendData);
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
