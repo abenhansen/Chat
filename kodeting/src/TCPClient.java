b7c6fcb6f076a83845d1b32dca36eef2dd88524b
Kenneth Leo Hansen <abenhansen@gmail.com>
2018-10-04 21:57:21 +0200

    Add files via upload

--- kodeting/src/TCPClient.java
+++ kodeting/src/TCPClient.java
@@ -9,51 +9,90 @@ import java.util.Scanner;
 public class TCPClient {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("What is your username?");
-        String Username;
 
         // Skal rykkes  til server
         /*while (!in.hasNext("[A-Za-z0-9\\-\\_]+")) {
             System.out.println("username needs to be lower than 12 characters and have no symbols, please try again");
             Username = in.nextLine();
         }*/
-        Username = in.nextLine();
+         //String Username = in.nextLine();
         /*while (Username.length() > 12) {
             System.out.println("username needs to be lower than 12 characters");
             Username = in.nextLine();
         }*/
         System.out.println("Enter server IP to connect to");
-        final String ipConnect = in.nextLine();
+      //  final String ipConnect = in.nextLine();
+        final String ipConnect2 = "127.0.0.1";
         System.out.println("Enter port port to connect to");
-        final int portConnect = in.nextInt();
+      //  final int portConnect = in.nextInt();
+        final int portConnect2 = 5659;
 
         try {
-            InetAddress ip = InetAddress.getByName(ipConnect);
+            InetAddress ip = InetAddress.getByName(ipConnect2);
             System.out.println("Connecting to the server");
-            Socket clientSocket = new Socket (ip, portConnect);
-
-            InputStream inp = clientSocket.getInputStream();
+            Socket clientSocket = new Socket (ip, portConnect2);
             OutputStream out = clientSocket.getOutputStream();
-            String userMsg = ("JOIN " + Username+ ", 127.0.0.1:5659\n");
-            byte[] userJoin = userMsg.getBytes();
-            out.write(userJoin);
 
-            in = new Scanner(System.in);
-            System.out.println("Message to server?");
-            String sendMessage = in.nextLine();
-            byte[] sendData = sendMessage.getBytes();
-            out.write(sendData);
+            System.out.println("What is your username?");
+            String Username = in.nextLine();
+           String userMsg = ("JOIN " + Username+ ", 127.0.0.1:5659\n");
+           byte[] userJoin = userMsg.getBytes();
+           out.write(userJoin);
 
-            byte[] dataRecieve = new byte[1024];
-            inp.read(dataRecieve);
-            String msgRecieve = new String(dataRecieve);
-            msgRecieve.trim();
+            Thread recieveThread = new Thread(()->{
+                try {
+                    clientMSG(clientSocket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            Thread sendThread = new Thread(()->{
+                try {
+                    clientSendMSG(clientSocket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            while(clientMSG(clientSocket)) {
+                sendThread.start();
+                recieveThread.start();
+            }
 
-            System.out.println("IN -->" + msgRecieve + "<--");
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+    public static boolean clientMSG (Socket clientSocket) throws IOException {
+        InputStream inp = clientSocket.getInputStream();
+        while(true){
+        byte[] dataRecieve = new byte[1024];
+        inp.read(dataRecieve);
+        String msgRecieve = new String(dataRecieve);
+        msgRecieve.trim();
+        System.out.println("IN -->" + msgRecieve + "<--");
+            return msgRecieve.trim().equals("J_OK");}
+    }
+
+    public static void clientSendMSG (Socket sock) throws IOException{
+        OutputStream out = sock.getOutputStream();
+        while(true){
+            Scanner in = new Scanner(System.in);
+            System.out.println("Message to server?");
+            String sendMessage = in.nextLine();
+            byte[] sendData = sendMessage.getBytes();
+            out.write(sendData);
+        }
+    }
+   /* public static boolean userAccept(Socket clientSocket) throws IOException {
+        InputStream inp = clientSocket.getInputStream();
+            byte[] dataRecieve = new byte[1024];
+            inp.read(dataRecieve);
+            String msgRecieve = new String(dataRecieve);
+            msgRecieve.trim();
+            System.out.println(msgRecieve);
+            return msgRecieve.equals("J_OK");
+        }*/
+
 }
--- kodeting/src/TCPServer.java
+++ kodeting/src/TCPServer.java
@@ -4,6 +4,7 @@ import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class TCPServer {
     public static void main(String[] args) {
         final int servport =5659;
@@ -13,41 +14,84 @@ public class TCPServer {
 
             System.out.println("Starting the server!\n");
 
-            Socket sock =  serv.accept();
-            System.out.println("User has connected!");
-            String userIP = sock.getInetAddress().getHostAddress();
-            InputStream inp = sock.getInputStream();
-            OutputStream out = sock.getOutputStream();
             while (true) {
+                Socket sock = serv.accept();
+                System.out.println("User has connected!");
+                String userIP = sock.getInetAddress().getHostAddress();
+
+                String serverMSG = "";
+
+                /*read sendThread = new Thread(() -> {
+                    try {
+                        serverMSG(sock, serverMSG);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                sendThread.start();*/
+
+
                 System.out.println("IP: " + userIP);
                 System.out.println("PORT: " + sock.getPort());
+                InputStream inp = sock.getInputStream();
                 byte[] userRecieve = new byte[1024];
                 inp.read(userRecieve);
-                String userResieveString = new String(userRecieve);
-                userResieveString.trim();
-                if (userResieveString.length() > 12) {
+                String userRecieveString = new String(userRecieve);
+                userRecieveString.trim();
+                String user_temp = userRecieveString.substring(5);
+                user_temp = user_temp.substring(0, user_temp.indexOf(","));
+                if (user_temp.length() > 12) {
+                    serverMSG(sock, "J_ER: Username is too long must be under 12 chars");
                     System.out.println("test");
                     continue;
-                } else if (userResieveString.contains("[A-Za-z0-9\\-\\_]")) {
+                }
+                if (checkChar(user_temp)) {
+                    serverMSG(sock, " J_ER: Username must not contain any symbols");
+                    System.out.println("test2");
                     continue;
-                } else
-                    System.out.println("Username: " + userResieveString);
-                break;
+                }
+                serverMSG(sock, "J_OK");
+                System.out.println("USER->" + user_temp + "<-USER");
+                Thread recieveThread = new Thread(() -> {
+                    try {
+                        serverRCVMSG(sock, userIP);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                recieveThread.start();
+                 }
 
-            }
+
+        }catch (IOException e) {
+            e.printStackTrace();
+        }}
+
+   public static boolean checkChar(String charr) {
+        String[] charrArray = (charr + "A").split("[^A-Za-z0-9\\-\\_]");
+        return charrArray.length != 1;
+    }
+
+    public static void serverMSG(Socket sock, String message) throws IOException{
+        OutputStream out = sock.getOutputStream();
+                byte[] sendData = message.getBytes();
+            out.write(sendData);
+    }
+
+    public static void serverRCVMSG (Socket sock, String userIP) throws IOException {
+        InputStream inp = sock.getInputStream();
+        while(true){
             byte[] dataRecieve = new byte[1024];
             inp.read(dataRecieve);
             String msgRecieve = new String(dataRecieve);
             msgRecieve.trim();
-
-            System.out.println("IN -->" + msgRecieve + "<--");
-
+            System.out.println("Server -->" + msgRecieve + "<--");
+            /*
             String sendMessage = "SERVER: [sender:" + userIP + " ]: " + msgRecieve;
-            byte[] sendData = sendMessage.getBytes();
-            out.write(sendData);
-        }catch (IOException e) {
-            e.printStackTrace();
+            serverMSG(sock, sendMessage);*/
         }
 
     }
+
+
 }
